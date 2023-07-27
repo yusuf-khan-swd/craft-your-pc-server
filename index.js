8db978f835c4c6a1e8360cc780dbfd12bc5702c1
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-27 22:26:52 +0600

    connect to mongodb server

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-
-console.log(process.env.DB_USER);
-console.log(process.env.DB_USER);
+const app = express();
 
 const port = process.env.PORT || 5000;
-
-const app = express();
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
 
 app.use(cors());
 app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${db_user}:${db_password}@cluster0.t99wqyy.mongodb.net/?retryWrites=true&w=majority"`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    console.log("Server successfully connected to MongoDB!");
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send({ message: "Server Api Working!!" });
