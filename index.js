2abdc35d3b2a780d8d0dd7e5e2b2ea501154da21
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-07-30 21:13:52 +0600

    remove the new keyword before ObjectID

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
-const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const port = process.env.PORT || 5000;
+
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-const db_user = process.env.DB_USER;
-const db_password = process.env.DB_PASS;
-
-const uri = `mongodb+srv://${db_user}:${db_password}@cluster0.t99wqyy.mongodb.net/?retryWrites=true&w=majority"`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t99wqyy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
-async function run() {
+const run = async () => {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    console.log("🛢 Server successfully connected to MongoDB!");
-
     const db = client.db("craft-your-pc");
     const productCollection = db.collection("products");
     const categoryCollection = db.collection("categories");
@@ -43,7 +33,7 @@ async function run() {
     app.get("/product/:id", async (req, res) => {
       const id = req.params.id;
       const result = await productCollection.findOne({
-        _id: new ObjectId(id),
+        _id: ObjectId(id),
       });
 
       res.status(200).send({ success: true, statusCode: 200, data: result });
@@ -59,7 +49,7 @@ async function run() {
       try {
         const id = req.params.id;
         const category = await categoryCollection.findOne({
-          _id: new ObjectId(id),
+          _id: ObjectId(id),
         });
 
         if (!category) {
@@ -103,24 +93,21 @@ async function run() {
     app.get("/category/:id", async (req, res) => {
       const id = req.params.id;
       const result = await categoryCollection.findOne({
-        _id: new ObjectId(id),
+        _id: ObjectId(id),
       });
 
       res.status(200).send({ success: true, statusCode: 200, data: result });
     });
   } finally {
   }
-}
-run().catch(console.dir);
+};
+
+run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send({ message: "CraftYourPC Server Api Working!!" });
-});
-
-app.get("/items", (req, res) => {
-  res.status(200).send({ success: true, statusCode: 200, data: [{}, {}] });
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
